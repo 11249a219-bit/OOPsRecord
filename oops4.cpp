6d4eeef836157dcabe9ec3e149d6592949c4ef89
11249a219-bit <11249a219@kanchiuniv.ac.in>
2025-11-07 21:06:07 +0530

    Add newline at end of oops4.cpp
    Fix missing newline at end of file.

---
+++ oops4.cpp
@@ -0,0 +1,49 @@
+Program 4: Employee Salary System
+Topics Covered: Constructor, Destructor, Static Member, Objects as Function Arguments, Array of Objects
+#include <iostream>
+using namespace std;
+
+class Employee {
+    string name;
+    double salary;
+    static int empCount;
+
+public:
+    Employee(string n, double s) {
+        name = n;
+        salary = s;
+        empCount++;
+    }
+
+    ~Employee() {
+        cout << name << " removed." << endl;
+    }
+
+    double getSalary() { return salary; }
+    string getName() { return name; }
+
+    static void showCount() {
+        cout << "Total Employees: " << empCount << endl;
+    }
+};
+
+// Compare salaries
+void compare(Employee e1, Employee e2) {
+    if(e1.getSalary() > e2.getSalary()) cout << e1.getName() << " earns more" << endl;
+    else cout << e2.getName() << " earns more" << endl;
+}
+
+int Employee::empCount = 0;
+
+int main() {
+    Employee e[3] = {
+        Employee("Ravi",50000),
+        Employee("Sita",60000),
+        Employee("Anu",55000)
+    };
+
+    compare(e[0], e[1]);
+    Employee::showCount();
+
+    return 0;
+}
