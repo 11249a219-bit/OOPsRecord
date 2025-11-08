af4fa033fdfe99af760f48436c0ab348b55afe48
11249a219-bit <11249a219@kanchiuniv.ac.in>
2025-11-07 22:18:21 -0800

    Add CarShowRoom.cpp to display car details

---
+++ CarShowRoom.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class car{
+    public:
+    string model;
+    string company;
+    float price;
+    void display(){
+        cout<<"company:"<<company
+        <<",model:"<<model
+        <<",price:Rs"<<price<<endl;
+    }
+};
+int main(){
+    car c1,c2,c3;
+    c1.company="toyota";
+    c1.model="Innova";
+    c2.price=2000000;
+    c2.company="honda";
+    c2.model="city";
+    c2.price=1200000;
+    c3.company="hyundai";
+    c3.model="izo";
+    c3.price=8000000;
+    c1.display();
+    c2.display();
+    c3.display();
+    return 0;
+}
